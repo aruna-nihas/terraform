c3e7ef532a9c7fc9b2b9159b9358f495449196bb
aruna <arunadegala18@gmail.com>
2024-04-04 10:31:43 +0530

    output

--- output/output.tf
+++ output/output.tf
@@ -1,10 +1,5 @@
-resource "aws_instance" "web" {
-    ami = "ami-0f3c7d07486cad139"
-    instance_type = "t2.micro"
-
-}
 output "instance_id" {
-    value = "aws_instance.web.id"
+    value = aws_instance.web.id
 
   
 }
@@ -13,10 +8,10 @@ output "instance_info" {
   
 }
 output "public_ip" {
-    value = "aws_instance.web.public_ip"
+    value = aws_instance.web.public_ip
   
 }
 output "private_ip" {
-    value = "aws_instance.web.private_ip"
+    value = aws_instance.web.private_ip
   
 }
